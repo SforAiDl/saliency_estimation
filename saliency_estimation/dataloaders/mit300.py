53cae103761b75334b3b97b9162109178fe882f7
Aditya Dubey <adityadubey16@gmail.com>
2022-08-24 23:39:24 +0530

    Added MIT300 dataloader

---
+++ saliency_estimation/dataloaders/mit300.py
@@ -0,0 +1,64 @@
+import os
+
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+
+class MIT300(Dataset):
+    """
+    Dataset class for loading MIT300 dataset
+    """
+
+    def __init__(self, csv_path, root_dir, transform=None) -> None:
+        """
+        init function
+        Args:
+        """
+        self.csv_path = csv_path
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        """
+        Return the length of the dataset
+        """
+        return len(self.csv_path)
+
+    def __getitem__(self, idx):
+        """
+        Return an item
+        Args:
+            index (int): index of the item
+        """
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = os.path.join(self.root_dir, self.csv_path.iloc[idx])
+        image = Image.open(img_path)
+
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+
+transform = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor()])
+
+
+def build_mit300(data_frame, root_dir, transform, num_workers, batch_size):
+    """
+    Return training and validation dataloader
+        Args:
+            #TODO
+    """
+    # Dataset
+    dataset = MIT300(data_frame, root_dir, transform)
+    # Dataloaders
+    test_dataloader = DataLoader(dataset, num_workers=num_workers, batch_size=batch_size)
+    return test_dataloader
+
+
+if __name__ == "__main__":
+    # TODO: testing the dataloader
+    pass
